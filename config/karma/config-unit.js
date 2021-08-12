5b393e69faa77b764bd2fa0f00dd784c26ee242b
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-08-12 23:48:19 +0200

    test(config): update Karma config

--- config/karma/config-unit.js
+++ config/karma/config-unit.js
@@ -12,7 +12,7 @@ module.exports = (config) => {
         client: {
             mocha: {
                 bail: true,
-                timeout: 10000
+                timeout: 20000
             }
         },
 
